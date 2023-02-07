4477a9f8d5135594adfd690326e8db3b14f332f7
Joao Raposo <raposo2281@gmail.com>
2023-02-07 12:40:43 +0000

    solved lab

---
+++ app.js
@@ -0,0 +1,27 @@
+//chamar o express package
+const express = require('express');
+
+//iniciar o express
+const app = express();
+
+//likar a pasta public para o servidor
+app.use(express.static('public'));
+
+
+//responde do servidor
+
+app.get('/home', (request, response) => {
+    response.sendFile(__dirname + '/views/home.html')
+})
+
+app.get('/about', (request, response) => {
+    response.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/works', (request, response) => {
+    response.sendFile(__dirname + '/views/works.html')
+})
+
+
+//listen do servidor
+app.listen(3000, () => console.log('Server running on port 3000'))
